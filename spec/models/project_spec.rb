aa43e852d1a1f70009b00e751df9f83251a2d3bc
dgoore <darius@campgurus.com>
2015-11-28 05:25:36 -0500

    add passing specs for projects model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Project do
+  it "is invalid without a name" do
+    project = build(:project, name: nil)
+    expect(project).to be_invalid
+  end
+  it "is invalid without a description" do
+    project = build(:project, description: nil)
+    expect(project).to be_invalid
+  end
+end
+
+describe "#new" do
+  it "takes two parameters and returns an Article object" do
+	project = build(:project)
+      project.should be_an_instance_of Project
+  end
+end
+
+describe "#name" do
+  it "returns the correct name" do
+  	project = build(:project)
+      expect(project.name).to include "MyString"
+  end
+end
+
+describe "#description" do
+  it "returns the correct description" do
+  	project = build(:project)
+      expect(project.description).to eq "MyText"
+  end
 end
